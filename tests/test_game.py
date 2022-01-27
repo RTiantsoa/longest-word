e228beee17d8cf13903884077fd3a0622958132f
Rajomarison tiantsoa M <rajomarisontiantsoa@gmail.com>
2022-01-27 16:00:43 +0300

    TDD: Check that attempt exists in the English dictionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -27,4 +27,7 @@ class TestGame(unittest.TestCase):
         self.assertIs(new_game.is_valid('SANDWICH'), False)
         self.assertEqual(new_game.grid, list('KWEUEAKRZ'))         
     
-    
+    def test_unknown_word_is_invalid(self):
+      new_game = Game()
+      new_game.grid = list('KWIENFUQW') # Forcer la grille à un scénario de test :
+      self.assertIs(new_game.is_valid('FEUN'), False)
